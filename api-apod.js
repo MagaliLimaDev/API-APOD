b5e34469434cbacdaa9e14978f56ca2a2d36a07b
MagaliLimaDev <mahrgfm@gmail.com>
2022-04-10 17:00:59 -0300

    Implementei o ajax caso a resposta da requisição for vídeo

--- api-apod.js
+++ api-apod.js
@@ -2,13 +2,19 @@ $("#submit").click(function (event) {
     event.preventDefault();
     let ResultadoDate = $("#date").val();
     buscarInfos(ResultadoDate);
-  });
-  function buscarInfos(ResultadoDate){
+});
+
+function buscarInfos(ResultadoDate) {
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=tS9F0K3v4W97bNw4yVK2ecVMogviTIwmshqXbaU9&date=${ResultadoDate}`,
         success: function (resposta) {
-          $("#mostra-title").text(resposta.title);
-          $("#mostra-foto").attr("src", resposta.url);
+            if (resposta.media_type == "image") {
+                $("#mostra-title").text(resposta.title);
+                $("#mostra-foto").attr("src", resposta.url);
+            } else {
+                $("#mostra-title").text(resposta.title);
+                $("#mostra-video").attr("src", resposta.url);
+            }
         },
-      });
-  }
+    });
+}
