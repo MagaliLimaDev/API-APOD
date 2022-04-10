10d523acebf535a1e8aeb69da38b9e0b05651066
MagaliLimaDev <mahrgfm@gmail.com>
2022-04-10 15:55:34 -0300

    arrumei o título, linkei o js e comecei a implementar a função que recebe a data informada pelo usuário

---
+++ api-apod.js
@@ -0,0 +1,8 @@
+$("#submit").click(function (event) {
+    event.preventDefault();
+    let date = $("#date").val();
+    buscarInfos(RasultadoDate);
+  });
+  function buscarInfos(RasultadoDate){
+    
+  }
